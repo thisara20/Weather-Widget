bf12b9cd4949a3a2211219641676b93903e71a7d
Thisara Chandrawansa <75584732+thisara20@users.noreply.github.com>
2024-02-25 01:02:51 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,37 +1,18 @@
-const express = require("express")
-//const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const dotenv = require("dotenv")
-const app = express()
-require("dotenv").config()
-const path = require("path")
+const express = require("express"); 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 8070
+const PORT = process.env.PORT || 8070;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-//const URL = process.env.MONGODB_URL
-
-// mongoose.connect(URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-
-// const connection = mongoose.connection
-// connection.once("open", () => {
-//   console.log("MongoDB connection success!")
-// })
-
-//give access to route file here
-//IT20197032
-
-
-const JobPostRouter = require("./Routes/Weather.js")
-app.use("/weather", JobPostRouter)
-//app.set('port', process.env.PORT || 3000)
+const WeatherRouter = require("./Routes/Weather.js");
+app.use("/weather", WeatherRouter); 
 
 app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}`)
-})
+  console.log(`Server up and running on port ${PORT}`);
+});
